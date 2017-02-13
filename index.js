dbe5d1008ab36e7ef9ba50f5f83e534f340c96e1
Dimitris Lamprinos <pkakelas@gmail.com>
2017-02-14 01:40:58 +0200

    Expand tabs to spaces

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var botkit = require('botkit');
 var fbConfig = require('config').get("facebook");
 
 var controller = botkit.facebookbot({
-	access_token: fbConfig.access_token,
-	verify_token: fbConfig.verify_token,
+  access_token: fbConfig.access_token,
+  verify_token: fbConfig.verify_token,
 });
 
 var bot = controller.spawn({
@@ -11,10 +11,10 @@ var bot = controller.spawn({
 
 controller.setupWebserver(8888,function(err,webserver) {
   controller.createWebhookEndpoints(controller.webserver, bot, function() {
-		console.log('This bot is online!!!');
+    console.log('This bot is online!!!');
   });
 });
 
 controller.hears(['hello'], 'message_received', function(bot, message) {
-	bot.reply(message, 'Hey there.');
+  bot.reply(message, 'Hey there.');
 });
